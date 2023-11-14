30617427ff678c3944ad382d0ca8ab5fe0e1fc43
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-14 17:42:56 +0530

    first commit

---
+++ css10.py
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<style>
+body
+{
+ background-color:yellow;
+}
+h4
+{
+ margin-left:400px;
+ margin-top:50px;
+ background-color:green;
+ color:yellow;
+ width:400px;
+ text-align:center;
+ padding:20px 20px 20px 20px;
+ box-shadow:4px 4px 4px 4px #000000;
+ border-style:double;
+ border-color:blue;
+ border-width:5px;
+}
+p
+{ 
+ margin-left:50px;
+ width:400px;
+ text-align:justify;
+ height:200px;
+ overflow:hidden;
+ padding:5px 5px 5px 5px;
+ font-family:verdana;
+ font-size:15px;
+}
+p:hover
+{
+ overflow:visible;
+}
+</style>
+</head>
+<body>
+ <h4>webpage</h4>
+  <p>a set of statement arranged in an order fashoin to get the result is called a program</p>
+</body>
+</html>
