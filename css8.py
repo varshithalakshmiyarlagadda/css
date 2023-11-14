7357b4f5766e7b30ca6fcd028cd35ae120072847
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-14 17:34:33 +0530

    first commit

---
+++ css8.py
@@ -0,0 +1,42 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<style type="text/css">
+body
+{
+background-color:green;
+}
+h5
+{
+  background-color:red;
+  color:yellow;
+  font-family:verdana;
+  text-decoration:underline;
+  text-align:center;
+}
+p
+{
+ background-color:cyan;
+ color:managenta;
+ font-size:20px;
+ font-family:impact;
+ font-weight:bolder;
+ text-align:justify;
+ world-spacing:2px;
+ text-transform:uppercase;
+ letter-spacing:2px;
+}
+</style>
+</head>
+<body>
+   <h5>COMPUTER PROGRAMMING</h5>
+  <p>
+    program is a set of statements arranged in an orderly fashion to get the acquired result.
+    program is a set of statements arranged in an orderly fashion to get the acquired result.
+    program is a set of statements arranged in an orderly fashion to get the acquired result.
+    program is a set of statements arranged in an orderly fashion to get the acquired result.
+    program is a set of statements arranged in an orderly fashion to get the acquired result.
+  </p>
+</body>
+</html> 
