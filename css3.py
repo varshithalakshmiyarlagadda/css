16950612f07c2cce246f886d151151d8fda0e7b2
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-14 16:58:52 +0530

    first commit

---
+++ css3.py
@@ -0,0 +1,16 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<meta charset="UTF-8">
+<style type="text/css">
+body
+{
+background-color:red;
+}
+</style>
+</head>
+<body>
+this text appears on red background
+</body>
+</html>
