77516ea3ea1b70bfe49152008002eded062eb793
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-14 16:46:20 +0530

    first commit

---
+++ css2.py
@@ -0,0 +1,11 @@
+<!doctype.html>
+<html>
+<head>
+<title>demo</title>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="mystyle.css">
+</head>
+<body>
+<h1>External css called</h1>
+</body>
+</html>
