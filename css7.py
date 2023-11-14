11bd898e13592e47893823f9062bb7c01bda2f5e
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-14 17:30:19 +0530

    first commit

---
+++ css7.py
@@ -0,0 +1,32 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<style type="text/css">
+body
+{
+background-color:green;
+}
+h5
+{
+  background-color:red;
+  color:yellow;
+  font-family:verdana;
+}
+p
+{
+ background-color:cyan;
+ color:managenta;
+ font-size:20px;
+ font-family:impact;
+ font-weight:bolder;
+}
+</style>
+</head>
+<body>
+   <h5>COMPUTER PROGRAMMING</h5>
+  <p>
+    program is a set of statements arranged in an orderly fashion to get the acquired result.
+  </p>
+</body>
+</html> 
