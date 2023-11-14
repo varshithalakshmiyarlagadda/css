c2ddd9aa106370927bf91b9755a6421503429fc5
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-14 17:38:16 +0530

    first commit

---
+++ css9.py
@@ -0,0 +1,72 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<style type="text/css">
+body
+{
+background-color:#E40BD4 
+}
+h4
+{
+ background-color:#0EEDC5;
+ text-align:center;
+ color:red;
+ font-family:verdana;
+ word-spacing:4px;
+ letter-spacing:4px;
+ width:400px;
+ height:25px;
+ margin-left:400px;
+ text-shadow:3px 3px 3px #000000;
+ box-shadow:2px 2px 2px #000000;
+ border-style:double;
+ border-color:magenta;
+ border-top-left-radius:40px;
+}
+.hs1
+{
+ background-color:#28C62D;
+ font-size:20px;
+ width:400px;
+ text-align:left;
+ text-transform:uppercase;
+}
+.ds
+{
+ background-color:green;
+ width:140px;
+ box-shadow:4px 4px 4px #000000;
+ border-style:double;
+ border-width:5px;
+ border-color:#485CAF;
+ outline-style:dashed;
+ outline-offset:15px;
+ outline-color:#57E40B;
+ margin-left:120px;
+}
+</style>
+</head>
+<body>
+   <h4>COMPUTER FUNDAMENTALS</h4>
+   <p class="hs1">computer</p><br>
+    computer is an electronic device which is used to perform arithmetical and logical operations.the word computer is derived from the greek word computer.
+    <p> class="hs1">computer basics</p>
+    <div class="ds">
+    <ul>
+        <li>speed</li>
+        <li>accuracy</li>
+        <li>deligence</li>
+        <li>versatility</li>
+        <li>reliability</li>
+        <li>automation</li>
+        <li>memory</li>
+    </ul>
+    </div>
+</body>
+</html>
+
+
+
+
+
