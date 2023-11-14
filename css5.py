024570d397a7dabc6735143ae9db1c284e42e71f
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-14 17:21:36 +0530

    first commit

---
+++ css5.py
@@ -0,0 +1,18 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<meta charset="UTF-8">
+<style type="text/css">
+body
+{
+background-image:url("file://c:/images/02.jpg");
+background-repeat:no-repeat;
+background-size:cover;
+}
+</style>
+</head>
+<body>
+this text appears on red background
+</body>
+</html>
