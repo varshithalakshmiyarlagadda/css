9e2f12622cd6a6ee614cea0b093555b0f32020dd
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-14 16:36:59 +0530

    first commit

---
+++ test1.py
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+    <head>
+        <title>css example</title>
+        <style type="text/css">
+              h1{background-color:red;}
+         </style>
+    </head>
+    <body>
+         <h1>CODING AT NIPUNA TECHNOLOGIES</h1>
+     </body>
+</html>
