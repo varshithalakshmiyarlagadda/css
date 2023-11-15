7db3901ba05711f72f76a02657dc44211f33791f
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-15 18:45:23 +0530

    first commit

---
+++ css12.py
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<meta charset="UTF-8">
+<style type="text/css">
+  .ust1
+  {
+    list-style-type:square
+  }
+   .ust2
+  {
+    list-style-type:circle
+  }
+</style>
+</head>
+<body>
+    <h4>INTODUCTON TO COMPUTERS</h4>
+    <ul class="ust1">
+        <li>speed</li>
+        <li>accuracy</li>
+        <li>diligence</li>
+        <li>iq</li>
+        <li>versatility</li>
+        <li>storage</li>
+   </ul>
+     <ul class="ust2">
+        <li>speed</li>
+        <li>accuracy</li>
+        <li>diligence</li>
+        <li>iq</li>
+        <li>versatility</li>
+        <li>storage</li>
+   </ul>
+      <ul class="ust1">
+        <li>speed</li>
+        <li>accuracy</li>
+        <li>diligence</li>
+        <li>iq</li>
+        <li>versatility</li>
+        <li>storage</li>
+   </ul>
+</body>
+</html> 
