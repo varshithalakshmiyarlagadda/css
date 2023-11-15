f9ea9d860943cc2252a5916f01463f596c79f15d
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-15 18:53:13 +0530

    first commit

---
+++ css14.py
@@ -0,0 +1,44 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<meta charset="UTF-8">
+<style type="text/css">
+  .ols1
+  {
+    list-style-type:upper-roman;
+  }
+   .ols2
+  {
+    list-style-type:lower-alpha;
+  }
+</style>
+</head>
+<body>
+    <h4>INTODUCTON TO COMPUTERS</h4>
+    <ol class="ols1">
+        <li>speed</li>
+        <li>accuracy</li>
+        <li>diligence</li>
+        <li>iq</li>
+        <li>versatility</li>
+        <li>storage</li>
+   </ol>
+   <ol class="ols2">
+        <li>speed</li>
+        <li>accuracy</li>
+        <li>diligence</li>
+        <li>iq</li>
+        <li>versatility</li>
+        <li>storage</li>
+   </ol>
+   <ol class="ols1">
+        <li>speed</li>
+        <li>accuracy</li>
+        <li>diligence</li>
+        <li>iq</li>
+        <li>versatility</li>
+        <li>storage</li>
+   </ol>
+</body>
+</html> 
