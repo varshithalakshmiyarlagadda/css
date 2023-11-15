76b5a7455b29f5569923b9ed8a842a407c46e023
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-15 18:49:41 +0530

    first commit

---
+++ css13.py
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<meta charset="UTF-8">
+<style type="text/css">
+  0l
+  {
+    list-style-type:upper-roman;
+  }
+</style>
+</head>
+<body>
+    <h4>INTODUCTON TO COMPUTERS</h4>
+    <ol>
+        <li>speed</li>
+        <li>accuracy</li>
+        <li>diligence</li>
+        <li>iq</li>
+        <li>versatility</li>
+        <li>storage</li>
+   </ol>
+</body>
+</html> 
