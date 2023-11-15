e750c9ec73cd33c057676ddced9bd5a945051e7c
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-15 18:40:01 +0530

    first commit

---
+++ css11.py
@@ -0,0 +1,24 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<meta charset="UTF-8">
+<style type="text/css">
+  ul
+  {
+    list-style-type:circle;
+  }
+</style>
+</head>
+<body>
+    <h4>INTODUCTON TO COMPUTERS</h4>
+    <ul>
+        <li>speed</li>
+        <li>accuracy</li>
+        <li>diligence</li>
+        <li>iq</li>
+        <li>versatility</li>
+        <li>storage</li>
+   </ul>
+</body>
+</html> 
