30f6c06d8849687e842f54951a45692a5bca9958
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-16 18:52:05 +0530

    first commit

---
+++ css19.py
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<meta charset="UTF-8">
+<style type="text/css">
+table,tr,td
+{
+border-collapse:collase;
+ border-style:double;
+ border-color:green;
+ width:200px;
+ height:40px;
+ background-color:red;
+}
+td
+{
+ text-align:center;
+ color:yellow;
+}
+</style>
+</head>
+<body>
+<table>
+   <tr><td>ENO</td><td>ENAME</td><td>ESAL</td><td>EGRADE</td></tr>
+   <tr><td>101</td><td>VARSHITHA</td><td>5500</td><td>A</td></tr>
+   <tr><td>102</td><td>DEVI</td><td>5000</td><td>B</td></tr>
+   <tr><td>103</td><td>RANI</td><td>4900</td><td>B</td></tr>
+</table>
+</dody>
+</html>
+   
+   
+ 
