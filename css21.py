a7227c61e4fad6b34ad9c849fcf5a0210bca7787
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-16 19:24:03 +0530

    first commit

---
+++ css21.py
@@ -0,0 +1,19 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8"
+<title>demo</title>
+<style>
+      p {
+             column-count: 3;
+             column-gap: 100px;
+             column-rule-width:5px;
+             column-rule-style:double;
+             column-rule-color:green;
+      }
+</style>
+</head>
+     <p>HTML, or HyperText Markup Language, is the standard markup language for creating and designing web pages. It structures the content on the web and uses a system of tags to define various elements such as headings, paragraphs, links, images, and more. HTML provides the basic structure for web pages, and it works in conjunction with other technologies like CSS (Cascading Style Sheets) and JavaScript to enhance the presentation and interactivity of web pages. In simpler terms, HTML is the skeleton that forms the backbone of most web content.
+</p>
+</body>
+</html>
