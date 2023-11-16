bbcf5b5ea0791833cdf20d083f19c94ceb78d82e
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-16 18:47:53 +0530

    first commit

---
+++ css18.py
@@ -0,0 +1,46 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<meta charset="UTF-8">
+<style>
+     a
+     {
+       text-decoration:none;
+       color:white;
+      }
+      ul
+      {
+       list-style-type:none;
+       word-spacing:15px;
+       line-height:30px;
+        }
+        li
+       {
+        dispaly:block;
+        }
+        .dl
+        {
+         background-color:black;
+         width:200px;
+         padding:30px 30px 30px 30px;
+         margin-left:400px;
+        }
+        a:hover
+       {
+        color:cyan;
+        background-color:blue;
+      }
+</style>
+</head>
+<body>
+    <div class="dl">
+    <ul>
+    <li><a href="http://www.indiatimes.com">INDIATIMES</a></li>
+    <li><a href="http://www.google.com">GOOGLE</a></li>
+    <li><a href="http://www.sakshi.com">SAKSHI</a></li>
+    <li><a href="http://www.andhrajyothy.com">ANDHRAJYOTHY</a></li>
+    </ul>
+</div>
+</body>
+</html>
