66511a74a2fc4a2a465747226e877855284a2b97
varshithalakshmiyarlagadda <varshithalakshmi27@gmail.com>
2023-11-16 18:36:16 +0530

    first commit

---
+++ css16.py
@@ -0,0 +1,34 @@
+<!doctype html>
+<html>
+<head>
+<title>demo</title>
+<meta charset="UTF-8">
+<style>
+a:link {
+   color:green;
+   background-color:transparent;
+   text-decoration:none;
+}
+a:visited {
+   color:pink;
+   background-color:transparent;
+   text-decoration:none;
+}
+a:hover {
+   color:red;
+   background-color:transparent;
+   text-decoration:underline;
+}
+a:active {
+   color:yellow;
+   background-color:transparent;
+   text-decoration:underline;
+}
+</style>
+</head>
+<body>
+<h2>Link Colors</h2>
+<p>you can change the default colors of links</p>
+<a href="http://www.google.com"target="_blank">GOOGLE</a>
+</body>
+</html>
